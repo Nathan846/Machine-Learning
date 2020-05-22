76ba5b600680d5721fe5ff19d7e96c4f9e342755
Nathan846 <54005418+Nathan846@users.noreply.github.com>
2020-05-23 00:36:32 +0530

    Multi-Linear Regression to predict cost of a house
    Given different features about a wide range of houses in a data-set, We try to predict the cost with certain features using Linear Regression.

---
+++ Multi-Linear Regression/gradientDescent.m
@@ -0,0 +1,20 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+% theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+% taking num_iters gradient steps with learning rate alpha
+
+m = length(y);
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+    d=(X*theta)-y;
+    k = 1:m;
+    t1 = sum((theta(1) + theta(2) .* X(k,2)) - y(k)); 
+    t2 = sum(((theta(1) + theta(2) .* X(k,2)) - y(k)) .* X(k,2));
+    theta(1) = theta(1)-(alpha/m)*(t1);
+    theta(2) = theta(2)-(alpha/m)*(t2);
+    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
