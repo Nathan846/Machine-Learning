fddddb042106b0200a5c78a85158e395f0fbd57e
Nathan846 <54005418+Nathan846@users.noreply.github.com>
2020-05-23 00:24:32 +0530

    Neural Networks-Feedforward
    Using the pre-calculated parameters of neural network, we attempt to build a digit recognition system using the text data set provided.

---
+++ Neural Network-FeedForward/predictOneVsAll.m
@@ -0,0 +1,16 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X.
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+p = zeros(size(X, 1), 1);
+
+X = [ones(m, 1) X];
+g=sigmoid(X*all_theta');
+[m,p]=max(g,[],2);
+
+end
